2af0e0c42388a2bcf35c55cc5ca1e0b73dd37c18
Christian <Christian@DESKTOP>
2016-08-14 15:53:18 +0200

    Feature: Update Nevit's Herald NPC to match High Five (CT2.6)
    -- Fixed License in file (L2J Server by L2J DataPack)
    -- Removed QuestState import, not's needed
    -- Added a minimal description of script
    -- Fixed spawns of NPC to retail values. Added missing spawn in Heine.
    Thanks to @maneco2 for coordinates
    -- Added comments where appropriate
    -- Removed ANTHARAS array and his "for each" loop. Thanks to @maneco2
    for his comment at respect.
    -- Moved SPAM array
    -- Edited onFirstTalk() method
    -- Removed check condition in onFirstTalk() method
    
    Reported By: @Sdw, @maneco2
    Patch By: @Sacrifice
    Tested By: @Sacrifice

--- dist/game/data/scripts/ai/npc/NevitsHerald/NevitsHerald.java
+++ dist/game/data/scripts/ai/npc/NevitsHerald/NevitsHerald.java
@@ -1,14 +1,14 @@
 /*
- * Copyright (C) 2004-2016 L2J Server
+ * Copyright (C) 2004-2016 L2J DataPack
  * 
- * This file is part of L2J Server.
+ * This file is part of L2J DataPack.
  * 
- * L2J Server is free software: you can redistribute it and/or modify
+ * L2J Datapack is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * L2J Server is distributed in the hope that it will be useful,
+ * L2J Datapack is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
@@ -26,7 +26,6 @@ import com.l2jserver.gameserver.model.L2World;
 import com.l2jserver.gameserver.model.actor.L2Npc;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
 import com.l2jserver.gameserver.model.effects.L2EffectType;
-import com.l2jserver.gameserver.model.quest.QuestState;
 import com.l2jserver.gameserver.network.NpcStringId;
 import com.l2jserver.gameserver.network.clientpackets.Say2;
 import com.l2jserver.gameserver.network.serverpackets.ExShowScreenMessage;
@@ -36,6 +35,7 @@ import com.l2jserver.util.Rnd;
 import ai.npc.AbstractNpcAI;
 
 /**
+ * Nevit's Herald AI.
  * @author Sacrifice
  */
 public final class NevitsHerald extends AbstractNpcAI
@@ -45,32 +45,27 @@ public final class NevitsHerald extends AbstractNpcAI
 	private static final int[][] NEVITS_HERALD_SPAWNS =
 	{
 		// @formatter:off
-		{86979, -142785, -1341, 18259},
-		{44168, -48513, -801, 31924},
-		{148002, -55279, -2735, 44315},
-		{147953, 26656, -2205, 20352},
-		{82313, 53280, -1496, 14791},
-		{81918, 148305, -3471, 49151},
-		{16286, 142805, -2706, 15689},
-		{-13968, 122050, -2990, 19497},
-		{-83207, 150896, -3129, 30709},
-		{116892, 77277, -2695, 45056}
+		{86971, -142772, -1336, 20480}, // Town of Schuttgart
+		{44165, -48494, -792, 32768}, // Rune Township
+		{148017, -55264, -2728, 49152}, // Town of Goddard
+		{147919, 26631, -2200, 16384}, // Town of Aden
+		{82325, 53278, -1488, 16384}, // Town of Oren
+		{81925, 148302, -3464, 49152}, // Town of Giran
+		{111678, 219197, -3536, 49152}, // Heine
+		{16254, 142808, -2696, 16384}, // Town of Dion
+		{-13865, 122081, -2984, 32768}, // Town of Gludio
+		{-83248, 150832, -3136, 32768}, // Gludin Village
+		{116899, 77256, -2688, 49152} // Hunters Village
 		// @formatter:on
 	};
-	private static final int[] ANTHARAS =
-	{
-		29019,
-		29066,
-		29067,
-		29068
-	};
+	private static final int ANTHARAS = 29068; // Antharas Strong (85)
+	private static final int VALAKAS = 29028; // Valakas (85)
 	private static final NpcStringId[] SPAM =
 	{
 		NpcStringId.SHOW_RESPECT_TO_THE_HEROES_WHO_DEFEATED_THE_EVIL_DRAGON_AND_PROTECTED_THIS_ADEN_WORLD,
 		NpcStringId.SHOUT_TO_CELEBRATE_THE_VICTORY_OF_THE_HEROES,
 		NpcStringId.PRAISE_THE_ACHIEVEMENT_OF_THE_HEROES_AND_RECEIVE_NEVITS_BLESSING
 	};
-	private static final int VALAKAS = 29028;
 	private static boolean isActive = false;
 	
 	private NevitsHerald()
@@ -81,22 +76,13 @@ public final class NevitsHerald extends AbstractNpcAI
 		addStartNpc(NEVITS_HERALD);
 		addTalkId(NEVITS_HERALD);
 		
-		for (int _npc : ANTHARAS)
-		{
-			addKillId(_npc);
-		}
+		addKillId(ANTHARAS);
 		addKillId(VALAKAS);
 	}
 	
 	@Override
 	public String onFirstTalk(L2Npc npc, L2PcInstance player)
 	{
-		QuestState st = player.getQuestState(getName());
-		
-		if (st == null)
-		{
-			st = newQuestState(player);
-		}
 		return "4326.htm";
 	}
 	
