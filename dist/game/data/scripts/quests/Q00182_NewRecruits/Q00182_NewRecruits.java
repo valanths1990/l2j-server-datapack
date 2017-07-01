da264c3c7a70f7b30715538098d5c28e746ed8e3
Adry85 <adrya85@hotmail.it>
2017-07-01 21:13:48 +0200

    Retail like quest New Recruits (182).

--- dist/game/data/scripts/quests/Q00182_NewRecruits/Q00182_NewRecruits.java
+++ dist/game/data/scripts/quests/Q00182_NewRecruits/Q00182_NewRecruits.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2016 L2J DataPack
+ * Copyright (C) 2004-2017 L2J DataPack
  * 
  * This file is part of L2J DataPack.
  * 
@@ -18,23 +18,29 @@
  */
 package quests.Q00182_NewRecruits;
 
+import com.l2jserver.gameserver.enums.CategoryType;
 import com.l2jserver.gameserver.enums.Race;
 import com.l2jserver.gameserver.model.actor.L2Npc;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
 import com.l2jserver.gameserver.model.quest.Quest;
 import com.l2jserver.gameserver.model.quest.QuestState;
-import com.l2jserver.gameserver.model.quest.State;
 
 /**
  * New Recruits (182)
- * @author Gnacik
- * @version 2010-10-15 Based on official server Naia
+ * @author Adry_85
+ * @since 2.6.0.0
  */
 public class Q00182_NewRecruits extends Quest
 {
 	// NPCs
 	private static final int KEKROPUS = 32138;
 	private static final int MENACING_MACHINE = 32258;
+	// Misc
+	private static final int MIN_LEVEL = 17;
+	private static final int MAX_LEVEL = 21;
+	// Rewards
+	private static final int RED_CRESCENT_EARRING = 10122;
+	private static final int RING_OF_DEVOTION = 10124;
 	
 	public Q00182_NewRecruits()
 	{
@@ -46,31 +52,61 @@ public class Q00182_NewRecruits extends Quest
 	@Override
 	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
 	{
-		String htmltext = event;
 		final QuestState st = getQuestState(player, false);
 		if (st == null)
 		{
-			return htmltext;
+			return null;
 		}
 		
-		if (npc.getId() == KEKROPUS)
+		String htmltext = null;
+		switch (event)
 		{
-			if (event.equalsIgnoreCase("32138-03.html"))
+			case "32138-03.htm":
 			{
-				st.startQuest();
+				if ((player.getLevel() >= MIN_LEVEL) && (player.getLevel() <= MAX_LEVEL) && player.isInCategory(CategoryType.FIRST_CLASS_GROUP))
+				{
+					htmltext = event;
+				}
+				break;
 			}
-		}
-		else if (npc.getId() == MENACING_MACHINE)
-		{
-			if (event.equalsIgnoreCase("32258-04.html"))
+			case "32138-04.htm":
 			{
-				st.giveItems(847, 2);
-				st.exitQuest(false, true);
+				if ((player.getLevel() >= MIN_LEVEL) && (player.getLevel() <= MAX_LEVEL) && player.isInCategory(CategoryType.FIRST_CLASS_GROUP))
+				{
+					st.startQuest();
+					st.setMemoState(1);
+					htmltext = event;
+				}
+				break;
 			}
-			else if (event.equalsIgnoreCase("32258-05.html"))
+			case "32258-02.html":
+			case "32258-03.html":
 			{
-				st.giveItems(890, 2);
-				st.exitQuest(false, true);
+				if (st.isMemoState(1))
+				{
+					htmltext = event;
+				}
+				break;
+			}
+			case "32258-04.html":
+			{
+				if (st.isMemoState(1))
+				{
+					giveItems(player, RED_CRESCENT_EARRING, 2);
+					st.exitQuest(false, true);
+					htmltext = event;
+				}
+				break;
+			}
+			case "32258-05.html":
+			{
+				if (st.isMemoState(1))
+				{
+					giveItems(player, RING_OF_DEVOTION, 2);
+					st.exitQuest(false, true);
+					htmltext = event;
+				}
+				break;
 			}
 		}
 		return htmltext;
@@ -79,47 +115,51 @@ public class Q00182_NewRecruits extends Quest
 	@Override
 	public String onTalk(L2Npc npc, L2PcInstance player)
 	{
-		String htmltext = getNoQuestMsg(player);
 		final QuestState st = getQuestState(player, true);
-		if (st == null)
+		String htmltext = getNoQuestMsg(player);
+		if (st.isCompleted())
 		{
-			return htmltext;
-		}
-		
-		final int npcId = npc.getId();
-		if (npcId == KEKROPUS)
-		{
-			switch (st.getState())
+			if (npc.getId() == KEKROPUS)
 			{
-				case State.CREATED:
-					final int level = player.getLevel();
-					if (player.getRace() == Race.KAMAEL)
-					{
-						htmltext = "32138-00.html";
-					}
-					else if ((level >= 17) && (level <= 21) && (player.getClassId().ordinal() == 0))
-					{
-						htmltext = "32138-01.htm";
-					}
-					else
-					{
-						htmltext = "32138-00b.html";
-					}
-					break;
-				case State.STARTED:
-					if (st.getInt("cond") == 1)
-					{
-						htmltext = "32138-04.html";
-					}
-					break;
-				case State.COMPLETED:
-					htmltext = getAlreadyCompletedMsg(player);
-					break;
+				htmltext = getAlreadyCompletedMsg(player);
 			}
 		}
-		else if ((npcId == MENACING_MACHINE) && st.isStarted())
+		else if (st.isCreated())
 		{
-			htmltext = "32258-01.html";
+			if (player.getRace() == Race.KAMAEL)
+			{
+				htmltext = "32138-01.htm";
+			}
+			else if ((player.getLevel() >= MIN_LEVEL) && (player.getLevel() <= MAX_LEVEL) && player.isInCategory(CategoryType.FIRST_CLASS_GROUP))
+			{
+				htmltext = "32138-02.htm";
+			}
+			else if ((player.getLevel() < MIN_LEVEL) || (player.getLevel() > MAX_LEVEL) || !player.isInCategory(CategoryType.FIRST_CLASS_GROUP))
+			{
+				htmltext = "32138-05.htm";
+			}
+		}
+		else if (st.isStarted())
+		{
+			switch (npc.getId())
+			{
+				case KEKROPUS:
+				{
+					if (st.isMemoState(1))
+					{
+						htmltext = "32138-06.html";
+					}
+					break;
+				}
+				case MENACING_MACHINE:
+				{
+					if (st.isMemoState(1))
+					{
+						htmltext = "32258-01.html";
+					}
+					break;
+				}
+			}
 		}
 		return htmltext;
 	}
