ab62d7284fe5319045838e98ef5294f41dc2f335
Adry85 <adrya85@hotmail.it>
2017-07-05 22:07:39 +0200

    Fixed Dragon Vortex AI. - Now spawned raid bosses are aggressive; - Added respawn delay;

--- dist/game/data/scripts/ai/npc/DragonVortex/DragonVortex.java
+++ dist/game/data/scripts/ai/npc/DragonVortex/DragonVortex.java
@@ -18,86 +18,134 @@
  */
 package ai.npc.DragonVortex;
 
-import ai.npc.AbstractNpcAI;
-
+import com.l2jserver.gameserver.model.Location;
 import com.l2jserver.gameserver.model.actor.L2Npc;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
 
+import ai.npc.AbstractNpcAI;
+
 /**
  * Dragon Vortex AI.
- * @author UnAfraid, improved by Adry_85
+ * @author Adry_85
+ * @since 2.6.0.0
  */
 public final class DragonVortex extends AbstractNpcAI
 {
 	// NPC
-	private static final int VORTEX = 32871;
+	private static final int DRAGON_VORTEX = 32871;
 	// Raids
-	private static final int[] RAIDS =
-	{
-		25718, // Emerald Horn
-		25719, // Dust Rider
-		25720, // Bleeding Fly
-		25721, // Blackdagger Wing
-		25722, // Shadow Summoner
-		25723, // Spike Slasher
-		25724, // Muscle Bomber
-	};
+	private static final int EMERALD_HORN = 25718;
+	private static final int DUST_RIDER = 25719;
+	private static final int BLEEDING_FLY = 25720;
+	private static final int BLACKDAGGER_WING = 25721;
+	private static final int SHADOW_SUMMONER = 25722;
+	private static final int SPIKE_SLASHER = 25723;
+	private static final int MUSCLE_BOMBER = 25724;
 	// Item
 	private static final int LARGE_DRAGON_BONE = 17248;
-	// Misc
-	private static final int DESPAWN_DELAY = 1800000; // 30min
+	// Variables
+	private static final String I_QUEST0 = "I_QUEST0";
+	// Location
+	private static final Location SPOT_1 = new Location(92744, 114045, -3072);
+	private static final Location SPOT_2 = new Location(110112, 124976, -3624);
+	private static final Location SPOT_3 = new Location(121637, 113657, -3792);
+	private static final Location SPOT_4 = new Location(109346, 111849, -3040);
 	
 	private DragonVortex()
 	{
 		super(DragonVortex.class.getSimpleName(), "ai/npc");
-		addStartNpc(VORTEX);
-		addFirstTalkId(VORTEX);
-		addTalkId(VORTEX);
+		addStartNpc(DRAGON_VORTEX);
+		addFirstTalkId(DRAGON_VORTEX);
+		addTalkId(DRAGON_VORTEX);
 	}
 	
 	@Override
 	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
 	{
-		if ("Spawn".equals(event))
+		switch (event)
 		{
-			if (hasQuestItems(player, LARGE_DRAGON_BONE))
+			case "RAIDBOSS":
 			{
-				takeItems(player, LARGE_DRAGON_BONE, 1);
-				final int random = getRandom(1000);
-				int raid = 0;
-				if (random < 292)
+				if (hasQuestItems(player, LARGE_DRAGON_BONE))
 				{
-					raid = RAIDS[0]; // Emerald Horn 29.2%
-				}
-				else if (random < 516)
-				{
-					raid = RAIDS[1]; // Dust Rider 22.4%
-				}
-				else if (random < 692)
-				{
-					raid = RAIDS[2]; // Bleeding Fly 17.6%
-				}
-				else if (random < 808)
-				{
-					raid = RAIDS[3]; // Blackdagger Wing 11.6%
-				}
-				else if (random < 900)
-				{
-					raid = RAIDS[4]; // Spike Slasher 9.2%
-				}
-				else if (random < 956)
-				{
-					raid = RAIDS[5]; // Shadow Summoner 5.6%
+					if (!npc.getVariables().getBoolean(I_QUEST0, false))
+					{
+						takeItems(player, LARGE_DRAGON_BONE, 1);
+						final int random = getRandom(100);
+						int raid = 0;
+						if (random < 3)
+						{
+							raid = MUSCLE_BOMBER;
+						}
+						else if (random < 8)
+						{
+							raid = SHADOW_SUMMONER;
+						}
+						else if (random < 15)
+						{
+							raid = SPIKE_SLASHER;
+						}
+						else if (random < 25)
+						{
+							raid = BLACKDAGGER_WING;
+						}
+						else if (random < 45)
+						{
+							raid = BLEEDING_FLY;
+						}
+						else if (random < 67)
+						{
+							raid = DUST_RIDER;
+						}
+						else
+						{
+							raid = EMERALD_HORN;
+						}
+						
+						Location LOC = null;
+						switch (npc.getX())
+						{
+							case 92225:
+							{
+								LOC = SPOT_1;
+								break;
+							}
+							case 110116:
+							{
+								LOC = SPOT_2;
+								break;
+							}
+							case 121172:
+							{
+								LOC = SPOT_3;
+								break;
+							}
+							case 108924:
+							{
+								LOC = SPOT_4;
+								break;
+							}
+						}
+						
+						npc.getVariables().set(I_QUEST0, true);
+						addSpawn(raid, LOC, false, 0, true);
+						startQuestTimer("CANSPAWN", 60000, npc, null);
+					}
+					else
+					{
+						return "32871-02.html";
+					}
 				}
 				else
 				{
-					raid = RAIDS[6]; // Muscle Bomber 4.4%
+					return "32871-01.html";
 				}
-				addSpawn(raid, npc.getX() + getRandom(-500, 500), npc.getY() + getRandom(-500, 500), npc.getZ() + 10, 0, false, DESPAWN_DELAY, true);
+				break;
 			}
-			else
+			case "CANSPAWN":
 			{
-				return "32871-no.html";
+				npc.getVariables().set(I_QUEST0, false);
+				break;
 			}
 		}
 		return super.onAdvEvent(event, npc, player);
