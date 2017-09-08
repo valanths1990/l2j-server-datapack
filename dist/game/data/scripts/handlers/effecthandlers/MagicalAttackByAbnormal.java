be0cee908dcb53f1dadea0b40cae43bde2de8fa5
Adry85 <adrya85@hotmail.it>
2017-09-08 21:18:53 +0200

    Removed power from all magic skills.

--- dist/game/data/scripts/handlers/effecthandlers/MagicalAttackByAbnormal.java
+++ dist/game/data/scripts/handlers/effecthandlers/MagicalAttackByAbnormal.java
@@ -25,6 +25,7 @@ import com.l2jserver.gameserver.model.conditions.Condition;
 import com.l2jserver.gameserver.model.effects.AbstractEffect;
 import com.l2jserver.gameserver.model.effects.L2EffectType;
 import com.l2jserver.gameserver.model.skills.BuffInfo;
+import com.l2jserver.gameserver.model.skills.Skill;
 import com.l2jserver.gameserver.model.stats.Formulas;
 import com.l2jserver.gameserver.model.stats.Stats;
 import com.l2jserver.util.Rnd;
@@ -35,9 +36,13 @@ import com.l2jserver.util.Rnd;
  */
 public final class MagicalAttackByAbnormal extends AbstractEffect
 {
+	private final double _power;
+	
 	public MagicalAttackByAbnormal(Condition attachCond, Condition applyCond, StatsSet set, StatsSet params)
 	{
 		super(attachCond, applyCond, set, params);
+		
+		_power = params.getDouble("power", 0);
 	}
 	
 	@Override
@@ -55,8 +60,9 @@ public final class MagicalAttackByAbnormal extends AbstractEffect
 	@Override
 	public void onStart(BuffInfo info)
 	{
-		L2Character target = info.getEffected();
-		L2Character activeChar = info.getEffector();
+		final L2Character target = info.getEffected();
+		final L2Character activeChar = info.getEffector();
+		final Skill skill = info.getSkill();
 		
 		if (activeChar.isAlikeDead())
 		{
@@ -68,11 +74,11 @@ public final class MagicalAttackByAbnormal extends AbstractEffect
 			target.stopFakeDeath(true);
 		}
 		
-		boolean sps = info.getSkill().useSpiritShot() && activeChar.isChargedShot(ShotType.SPIRITSHOTS);
-		boolean bss = info.getSkill().useSpiritShot() && activeChar.isChargedShot(ShotType.BLESSED_SPIRITSHOTS);
-		final boolean mcrit = Formulas.calcMCrit(activeChar.getMCriticalHit(target, info.getSkill()));
-		final byte shld = Formulas.calcShldUse(activeChar, target, info.getSkill());
-		int damage = (int) Formulas.calcMagicDam(activeChar, target, info.getSkill(), shld, sps, bss, mcrit);
+		boolean sps = skill.useSpiritShot() && activeChar.isChargedShot(ShotType.SPIRITSHOTS);
+		boolean bss = skill.useSpiritShot() && activeChar.isChargedShot(ShotType.BLESSED_SPIRITSHOTS);
+		final boolean mcrit = Formulas.calcMCrit(activeChar.getMCriticalHit(target, skill));
+		final byte shld = Formulas.calcShldUse(activeChar, target, skill);
+		double damage = Formulas.calcMagicDam(activeChar, target, skill, shld, sps, bss, mcrit, _power);
 		
 		// each buff increase +30%
 		damage *= (((target.getBuffCount() * 0.3) + 1.3) / 4);
@@ -87,16 +93,16 @@ public final class MagicalAttackByAbnormal extends AbstractEffect
 			}
 			
 			// Shield Deflect Magic: Reflect all damage on caster.
-			if (target.getStat().calcStat(Stats.VENGEANCE_SKILL_MAGIC_DAMAGE, 0, target, info.getSkill()) > Rnd.get(100))
+			if (target.getStat().calcStat(Stats.VENGEANCE_SKILL_MAGIC_DAMAGE, 0, target, skill) > Rnd.get(100))
 			{
-				activeChar.reduceCurrentHp(damage, target, info.getSkill());
-				activeChar.notifyDamageReceived(damage, target, info.getSkill(), mcrit, false, true);
+				activeChar.reduceCurrentHp(damage, target, skill);
+				activeChar.notifyDamageReceived(damage, target, skill, mcrit, false, true);
 			}
 			else
 			{
-				target.reduceCurrentHp(damage, activeChar, info.getSkill());
-				target.notifyDamageReceived(damage, activeChar, info.getSkill(), mcrit, false, false);
-				activeChar.sendDamageMessage(target, damage, mcrit, false, false);
+				target.reduceCurrentHp(damage, activeChar, skill);
+				target.notifyDamageReceived(damage, activeChar, skill, mcrit, false, false);
+				activeChar.sendDamageMessage(target, (int) damage, mcrit, false, false);
 			}
 		}
 	}
