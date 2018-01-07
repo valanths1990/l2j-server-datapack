46143290127e24a9b44f7884b9bf15213ea34fa9
Adry85 <adrya85@hotmail.it>
2018-01-07 12:44:12 +0100

    Retail like skill Sublime Self-Sacrifice (1505).
    Required
    https://bitbucket.org/l2jserver/l2j_server/commits/9a04bcd985b94b0a291452f9aaf270e2b4f1f712
    
    Patch by: Sahar Atias

--- dist/game/data/scripts/handlers/MasterHandler.java
+++ dist/game/data/scripts/handlers/MasterHandler.java
@@ -221,6 +221,7 @@ import handlers.targethandlers.AreaFriendly;
 import handlers.targethandlers.AreaSummon;
 import handlers.targethandlers.Aura;
 import handlers.targethandlers.AuraCorpseMob;
+import handlers.targethandlers.AuraFriendly;
 import handlers.targethandlers.BehindArea;
 import handlers.targethandlers.BehindAura;
 import handlers.targethandlers.Clan;
@@ -544,6 +545,7 @@ public class MasterHandler
 			AreaSummon.class,
 			Aura.class,
 			AuraCorpseMob.class,
+			AuraFriendly.class,
 			BehindArea.class,
 			BehindAura.class,
 			Clan.class,
