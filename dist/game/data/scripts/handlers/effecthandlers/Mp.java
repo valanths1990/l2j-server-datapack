b995d4fcab87ade64b2cb8740ad4580bd4a7da12
Adry85 <adrya85@hotmail.it>
2018-02-23 15:27:15 +0100

    Reworked instant MP effect.

--- dist/game/data/scripts/handlers/effecthandlers/Mp.java
+++ dist/game/data/scripts/handlers/effecthandlers/Mp.java
@@ -23,33 +23,28 @@ import com.l2jserver.gameserver.model.StatsSet;
 import com.l2jserver.gameserver.model.actor.L2Character;
 import com.l2jserver.gameserver.model.conditions.Condition;
 import com.l2jserver.gameserver.model.effects.AbstractEffect;
-import com.l2jserver.gameserver.model.effects.L2EffectType;
 import com.l2jserver.gameserver.model.skills.BuffInfo;
+import com.l2jserver.gameserver.model.skills.Skill;
+import com.l2jserver.gameserver.model.stats.Stats;
 import com.l2jserver.gameserver.network.SystemMessageId;
 import com.l2jserver.gameserver.network.serverpackets.SystemMessage;
 
 /**
- * Hp effect implementation.
+ * Mp effect implementation.
  * @author Adry_85
  * @since 2.6.0.0
  */
-public final class Hp extends AbstractEffect
+public final class Mp extends AbstractEffect
 {
 	private final double _amount;
 	private final EffectCalculationType _mode;
 	
-	public Hp(Condition attachCond, Condition applyCond, StatsSet set, StatsSet params)
+	public Mp(Condition attachCond, Condition applyCond, StatsSet set, StatsSet params)
 	{
 		super(attachCond, applyCond, set, params);
 		
 		_amount = params.getDouble("amount", 0);
 		_mode = params.getEnum("mode", EffectCalculationType.class, EffectCalculationType.DIFF);
-	}
-	
-	@Override
-	public L2EffectType getEffectType()
-	{
-		return L2EffectType.HP;
 	}
 	
 	@Override
@@ -63,7 +58,7 @@ public final class Hp extends AbstractEffect
 	{
 		final L2Character target = info.getEffected();
 		final L2Character activeChar = info.getEffector();
-		if ((target == null) || target.isDead() || target.isDoor() || target.isInvul() || target.isHpBlocked())
+		if ((target == null) || target.isDead() || target.isDoor() || target.isInvul() || target.isMpBlocked())
 		{
 			return;
 		}
@@ -73,41 +68,59 @@ public final class Hp extends AbstractEffect
 		{
 			case DIFF:
 			{
-				amount = Math.min(_amount, target.getMaxRecoverableHp() - target.getCurrentHp());
+				if (_amount < 0)
+				{
+					amount = _amount;
+				}
+				else
+				{
+					final Skill skill = info.getSkill();
+					if (!skill.isStatic())
+					{
+						amount = target.calcStat(Stats.MANA_CHARGE, _amount, null, null);
+					}
+					
+					amount = Math.min(skill.isStatic() ? _amount : amount, target.getMaxRecoverableMp() - target.getCurrentMp());
+				}
 				break;
 			}
 			case PER:
 			{
 				if (_amount < 0)
 				{
-					amount = (target.getCurrentHp() * _amount) / 100;
+					amount = (target.getCurrentMp() * _amount) / 100;
 				}
 				else
 				{
-					amount = Math.min((target.getMaxHp() * _amount) / 100.0, target.getMaxRecoverableHp() - target.getCurrentHp());
+					amount = Math.min((target.getMaxMp() * _amount) / 100.0, target.getMaxRecoverableMp() - target.getCurrentMp());
 				}
 				break;
 			}
 		}
 		
-		if (amount != 0)
-		{
-			target.setCurrentHp(amount + target.getCurrentHp());
-		}
-		
 		if (amount >= 0)
 		{
+			if (amount != 0)
+			{
+				target.setCurrentMp(amount + target.getCurrentMp());
+			}
+			
+			SystemMessage sm;
 			if ((activeChar != null) && (activeChar != target))
 			{
-				final SystemMessage sm = SystemMessage.getSystemMessage(SystemMessageId.S2_HP_HAS_BEEN_RESTORED_BY_C1);
-				sm.addCharName(activeChar);
+				sm = SystemMessage.getSystemMessage(SystemMessageId.S2_MP_HAS_BEEN_RESTORED_BY_C1);
+				sm.addCharName(info.getEffector());
 				sm.addInt((int) amount);
 				target.sendPacket(sm);
 			}
 			else
 			{
-				target.sendPacket(SystemMessage.getSystemMessage(SystemMessageId.S1_HP_HAS_BEEN_RESTORED).addInt((int) amount));
+				target.sendPacket(SystemMessage.getSystemMessage(SystemMessageId.S1_MP_HAS_BEEN_RESTORED).addInt((int) amount));
 			}
+		}
+		else
+		{
+			target.reduceCurrentMp(Math.abs(amount));
 		}
 	}
 }
