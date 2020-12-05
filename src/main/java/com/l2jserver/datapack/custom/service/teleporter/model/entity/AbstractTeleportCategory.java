287e3abbabdfc16f37fdcfb8bad937fc435e0cae
HorridoJoho <HorridoJoho@l2jserver.com>
2020-12-05 12:25:28 +0100

    TeleporterService
    Add TeleporterService. Needs game update for configs. Please enable in
    teleporterservice.properties in game.
    
    This also needs custom npc data to be enabled in general.properties.
    
    The default NPC is 60002. So when the teleporter is loaded you first
    need to spawn it with the //spawn 60002 command.
    
    Thanks to: @Marduk13

--- src/main/java/com/l2jserver/datapack/custom/service/teleporter/model/entity/AbstractTeleportCategory.java
+++ src/main/java/com/l2jserver/datapack/custom/service/teleporter/model/entity/AbstractTeleportCategory.java
@@ -16,37 +16,26 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.l2jserver.datapack.custom.service.base.model.entity;
+package com.l2jserver.datapack.custom.service.teleporter.model.entity;
 
-import java.util.List;
-
-import com.l2jserver.datapack.custom.service.base.util.htmltmpls.HTMLTemplatePlaceholder;
+import com.l2jserver.datapack.custom.service.base.model.entity.Refable;
+import com.l2jserver.datapack.custom.service.teleporter.model.TeleporterConfig;
 
 /**
- * Custom Service Product.
+ * Base class for teleporter categories.
  * @author HorridoJoho
  * @version 2.6.2.0
  */
-public abstract class CustomServiceProduct extends Refable {
-	private List<ItemRequirement> items;
+public abstract class AbstractTeleportCategory extends Refable {
+	private String name;
 	
-	protected CustomServiceProduct() {
-	}
-	
-	@Override
-	public void afterDeserialize() {
+	public void afterDeserialize(TeleporterConfig config) {
 		super.afterDeserialize();
 		
-		if (!items.isEmpty()) {
-			HTMLTemplatePlaceholder itemsPlaceholder = getPlaceholder().addChild("items", null).getChild("items");
-			for (ItemRequirement item : items) {
-				item.afterDeserialize();
-				itemsPlaceholder.addAliasChild(String.valueOf(itemsPlaceholder.getChildrenSize()), item.getPlaceholder());
-			}
-		}
+		getPlaceholder().addChild("name", name);
 	}
 	
-	public final List<ItemRequirement> getItems() {
-		return items;
+	public final String getName() {
+		return name;
 	}
 }
